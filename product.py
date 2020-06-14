26a4b479c39a527666b40fc550c716bbd1fd2b9c
Mark Tseng <amaranthzazaa@hotmail.com>
2020-06-14 15:12:52 +0800

    price new version

--- product.py
+++ product.py
@@ -15,3 +15,8 @@ print (products[0][1])
 
 for k in products:
 	print (k[0], '的價格為', k[1])
+
+
+with open ('products.csv', 'w') as f:
+	for k in products:
+		f.write(k[0] + ',' + k[1] + '\n')  #加入逗點方便CSV檔閱讀，寫入最後一定要用\n做分行
